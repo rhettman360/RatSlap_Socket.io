c76a9d56d87a7ec45c120efa5cfd0f9949fc3e82
Kendra R <ramirezskendra@gmail.com>
2017-10-06 12:26:34 -0700

    first commit 2

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
-const express = require("express")
-const path = require("path")
+var express = require ("express")
+var session = require('express-session')
+var path = require("path")
+var app = express()
+var bodyParser = require('body-parser');
+var PORT = 8000
 
-const app = express()
-
-const PORT = 8000
-
+app.use(session({secret: 'codingdojorocks'}));  // string for encryption
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
 app.use(express.static(path.join(__dirname, "./client")))
+app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "./node_modules")))
-
-const server = app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`)
+const server = app.listen(PORT, () =>{
+  console.log(`Listening on port ${PORT}`)
 })
-
 const io = require("socket.io").listen(server)
 
-let click = 0
-
-io.sockets.on("connection", socket => {
-	console.log("New connection", socket.id)
-
-	socket.emit("counter", click)
-
-	socket.on("some sort of click counter", () => {
-		click++
-		io.emit("counter", click)
-	})
-
+app.get("/", (req, res) =>{
+  if (req.session && !req.session.name) {
+    req.session.name = ""
+  }
+  console.log(req.session.name);
+  res.render("index", req.session)
 })
+
+app.post("/startGame", (req, res)=>{
+  req.session.name = req.body.name
+  console.log("it worked!", req.session.name);
+  res.render("game", req.session)
+})
+
+// io.sockets.on("connection", socket =>{
+//   console.log("New Connection", socket.id)
+//   socket.emit("counter", click)
+//   socket.on("clickcounter", () => {
+//     click++
+//     io.emit("counter", click)
+//   })
+// })
